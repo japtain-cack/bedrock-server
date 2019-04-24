259da429271c194e8addfc39f7406c4dfb15341f
Nathan Snow <nathan.snow@mimir-tech.org>
2019-04-24 14:13:59 -0600

    completing build script

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,63 @@
-FROM ubuntu:latest
+FROM ubuntu:bionic
+LABEL author="Nathan Snow"
+LABEL description="Minecraft Pocket Edition (Minecraft PE or Minecraft Bedrock) server"
 
-RUN apt-get update
-RUN apt-get install -y unzip curl libcurl4 libssl1.0.0
-RUN curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.11.0.23.zip --output bedrock-server.zip
-RUN unzip bedrock-server.zip -d bedrock-server
-RUN rm bedrock-server.zip
-
-WORKDIR /bedrock-server
+WORKDIR /minecraft
+ARG REV=1.11.0.23
 ENV LD_LIBRARY_PATH=.
-CMD ./bedrock_server
+ENV MCPE_HOME=/minecraft
+ENV BRSRVDIR=bedrock-server-$REV
+ENV UID=1000
+ENV GUID=1000
+ENV LEVEL=world \
+    PVP=true \
+    VDIST=10 \
+    OPPERM=4 \
+    NETHER=true \
+    FLY=false \
+    MAXBHEIGHT=256 \
+    NPCS=true \
+    WLIST=false \
+    ANIMALS=true \
+    HC=false \
+    ONLINE=false \
+    RPACK='' \
+    DIFFICULTY=3 \
+    CMDBLOCK=false \
+    MAXPLAYERS=20 \
+    MONSTERS=true \
+    STRUCTURES=true \
+    SPAWNPROTECTION=16 \
+    MODE=0 \
+    CHEATS=false \
+    SERVERNAME=dedicated-server
+
+RUN apt-get -y update && apt-get -y install \
+    sudo \
+    unzip \
+    curl \
+    libcurl4 \
+    libssl1.0.0
+
+RUN curl https://minecraft.azureedge.net/bin-linux/bedrock-server-${REV}.zip --output /tmp/${BRSRVDIR}.zip && \
+    unzip /tmp/${BRSRVDIR}.zip -d /tmp/${BRSRVDIR} && \
+    rm -fv /tmp/${BRSRVDIR}.zip
+
+RUN groupadd -g $GUID minecraft && \
+    useradd -s /bin/bash -d /minecraft -m minecraft -u $UID -g minecraft && \
+    usermod -aG sudo minecraft
+
+ADD ./scripts/run.sh /tmp/
+RUN chmod +x /tmp/run.sh
+
+VOLUME ["/minecraft"]
+
+USER root
+
+EXPOSE 19132/tcp
+EXPOSE 19132/udp
+EXPOSE 19133/tcp
+EXPOSE 19133/udp
+
+CMD trap 'exit' INT; /tmp/run.sh
+
---
+++ scripts/run.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+# Borrowed some snippets from AshDevFr/docker-spigot, thanks AshDevFr!
+
+echo "MCPE_HOME: $MCPE_HOME"
+echo "BRSRVDIR: $BRSRVDIR"
+
+# Copy game files to working dir
+cp -rf /tmp/${BRSRVDIR}/* ./ \
+  && rm -rf /tmp/${BRSRVDIR} \
+
+# Change owner to minecraft.
+if [ "$SKIPCHMOD" != "true" ]; then
+  sudo chown -R minecraft:minecraft $MCPE_HOME/
+else
+  echo "SKIPCHMOD option enabled. If you have access issue with your files, disable it"
+fi
+
+# Some variables are mandatory.
+if [ -z "$REV" ]; then
+  echo "REV option not defined. Please specify the version of the minecraft bedrock server."
+fi
+
+function setServerProp {
+  local prop=$1
+  local var=$2
+  if [ -n "$var" ]; then
+    echo "Setting $prop to $var"
+    sed -i "/$prop\s*=/ c $prop=$var" $MCPE_HOME/server.properties
+  fi
+}
+
+if [ -f $MCPE_HOME/server.properties ]; then
+  setServerProp "motd" "$MOTD"
+  setServerProp "level-name" "$LEVEL"
+  setServerProp "level-seed" "$SEED"
+  setServerProp "pvp" "$PVP"
+  setServerProp "view-distance" "$VDIST"
+  setServerProp "op-permission-level" "$OPPERM"
+  setServerProp "allow-nether" "$NETHER"
+  setServerProp "allow-flight" "$FLY"
+  setServerProp "max-build-height" "$MAXBHEIGHT"
+  setServerProp "spawn-npcs" "$NPCS"
+  setServerProp "white-list" "$WLIST"
+  setServerProp "spawn-animals" "$ANIMALS"
+  setServerProp "hardcore" "$HC"
+  setServerProp "online-mode" "$ONLINE"
+  setServerProp "resource-pack" "$RPACK"
+  setServerProp "difficulty" "$DIFFICULTY"
+  setServerProp "enable-command-block" "$CMDBLOCK"
+  setServerProp "max-players" "$MAXPLAYERS"
+  setServerProp "spawn-monsters" "$MONSTERS"
+  setServerProp "generate-structures" "$STRUCTURES"
+  setServerProp "spawn-protection" "$SPAWNPROTECTION"
+  setServerProp "max-tick-time" "$MAX_TICK_TIME"
+  setServerProp "max-world-size" "$MAX_WORLD_SIZE"
+  setServerProp "resource-pack-sha1" "$RPACK_SHA1"
+  setServerProp "network-compression-threshold" "$NETWORK_COMPRESSION_THRESHOLD"
+  setServerProp "allow-cheats" "$CHEATS"
+  setServerProp "server-name" "$SERVERNAME"
+
+  if [ -n "$MODE" ]; then
+    case ${MODE,,?} in
+      0|1|2|3)
+        ;;
+      s*)
+        MODE=0
+        ;;
+      c*)
+        MODE=1
+        ;;
+      *)
+        echo "ERROR: Invalid game mode: $MODE"
+        exit 1
+        ;;
+    esac
+
+    sed -i "/gamemode\s*=/ c gamemode=$MODE" $MCPE_HOME/server.properties
+  fi
+else
+  echo
+  echo "server.properties not found!"
+  exit 1
+fi
+
+if [ -n "$OPS" -a ! -e $MCPE_HOME/ops.txt.converted ]; then
+  echo $OPS | awk -v RS=, '{print}' > $MCPE_HOME/ops.txt
+fi
+
+./bedrock_server
+
+echo "code: $?"
+
